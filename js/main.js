05f37c3b5423f99b3bfc08ab5277fb1b2118aac4
ImagineUnlimitedGroup <imagineunlimitedgroup@gmail.com>
2024-02-13 19:50:37 -0700

    066

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ function initMap() {
 	var uluru = {lat: 39.6668716, lng: -104.8602724};
 
 	let map = new google. maps. Map (document. getElementById ('map'), {
-		mapId: "e31bdd6c7d81467e",
-		zoom: 17.5, 
+		mapId: "d2e5d72082875b71",
+		zoom: 16, 
 		disableDefaultUI: true,
 		center: uluru
 	});
